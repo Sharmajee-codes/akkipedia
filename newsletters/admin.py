5c688eeeba31b096d0671e11ad84117a23be3f76
Ankit Sharma <theankitsharma13@gmail.com>
2017-09-21 01:05:00 +0530

    v

--- newsletters/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import NewsletterUser
-
-class NewsletterAdmin(admin.ModelAdmin):
-	list_display = ('email', 'date_added', )
-
-admin.site.register(NewsletterUser, NewsletterAdmin)
